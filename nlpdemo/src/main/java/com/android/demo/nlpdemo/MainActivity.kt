6a33c958514c6d57a011b1a6c9124e11a90b1ec4
fenghongxiang <fenghongxiang@xiaomi.com>
2022-10-26 11:10:30 +0800

    [Feature]增加兼容性解析逻辑

--- nlpdemo/src/main/java/com/android/demo/nlpdemo/MainActivity.kt
+++ nlpdemo/src/main/java/com/android/demo/nlpdemo/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hankcs.hanlp.HanLP
 import com.hankcs.hanlp.seg.common.Term
 import dalvik.system.DexClassLoader
+//import jackmego.com.jieba_android.JiebaSegmenter
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,38 +42,60 @@ class MainActivity : AppCompatActivity() {
         val methodLoadFile = File(App.application.cacheDir.absolutePath + File.separator + "testjar.jar")
         val classLoader = DexClassLoader(methodLoadFile.absolutePath, optimizedDexOutputPath.absolutePath, null, classLoader)
 
-        val decoderClass = classLoader.loadClass("com.example.testjar.Decode")
-        val getResMethod = decoderClass.getDeclaredMethod("getRes")
-        var invoke: String? = getResMethod.invoke(null) as String?
-
-        Toast.makeText(this, invoke, Toast.LENGTH_SHORT).show()
+//        val decoderClass = classLoader.loadClass("com.example.testjar.Decode")
+//        val getResMethod = decoderClass.getDeclaredMethod("getRes")
+//        var invoke: String? = getResMethod.invoke(null) as String?
+//
+//        Toast.makeText(this, invoke, Toast.LENGTH_SHORT).show()
 
         decoder = CsvDecoder()
 
         decoder.setFilePath(path + File.separator + "ct.csv")
 
-        decoder.decodeLines(500).forEach { decodeResultList.add(DecoderBean(it, null)) }
+        decodeResultList.clear()
+
+        val decodeLines = decoder.decodeLines(500)
+        decodeLines.forEach { decodeResultList.add(DecoderBean(it, null)) }
 
         decodeResultList.forEach {
 
             val segment: List<Term?> = HanLP.segment(it.smsContent)
 //                    val dividedString = JiebaSegmenter.getJiebaSegmenterSingleton().getDividedString(it.smsContent)
 
+            if (it.smsContent.contains("0.75元。直接回复HF查询实时")) {
+                println("好耶")
+            }
+
             var decorderResult: String? = null
 
             for (termWithIndex in segment.withIndex()) {
                 if (termWithIndex.value?.nature?.toString() == "m") {       //数量词 m
+
+                    var numberStr = termWithIndex.value?.word
+
                     var indexTmp = termWithIndex.index - 1
+
+                    if (segment.getOrNull(termWithIndex.index - 1).let { it?.nature.toString() == "nx" && it?.word == "-" }) {
+                        numberStr = "-$numberStr"
+                        indexTmp--
+                    }
+
                     var finded = false;
-                    while (indexTmp > 0) {
+                    while (indexTmp >= 0) {
                         if (segment[indexTmp]?.nature.toString() == "w") {      //标点符号
                             indexTmp--
                         } else if (segment[indexTmp]?.nature.toString() == "p" && segment[indexTmp]?.word in arrayOf("是", "为")) {
                             indexTmp--
-                        } else if (segment[indexTmp]?.nature.toString() == "n" && segment[indexTmp]?.word in arrayOf("余额", "欠费", "结余")) {        //余额 名词n
+                        } else if (segment[indexTmp]?.nature.toString() in arrayOf("n", "vn") && segment[indexTmp]?.word in arrayOf("余额", "结余")) {        //余额 名词n
                             finded = true
                             break
-                        } else if (segment[indexTmp]?.nature.toString() == "n" && segment[indexTmp]?.word in arrayOf("剩余")) {
+                        } else if (segment[indexTmp]?.nature.toString() in arrayOf("n", "vn") && segment[indexTmp]?.word in arrayOf("欠费")) {
+                            if (numberStr?.contains("") == false) {
+                                numberStr = "-$numberStr"
+                            }
+                            finded = true
+                            break
+                        } else if (segment[indexTmp]?.nature.toString() in arrayOf("n", "vn") && segment[indexTmp]?.word in arrayOf("剩余")) {
                             if (indexTmp - 1 > 0 && segment[indexTmp - 1]?.nature.toString() == "n" && segment[indexTmp - 1]?.word.toString() == "话费") {
                                 finded = true
                                 break
@@ -84,14 +108,14 @@ class MainActivity : AppCompatActivity() {
                     }
                     if (finded) {
                         if (termWithIndex.index + 1 < segment.size && segment[termWithIndex.index + 1]?.nature?.toString() == "q" && segment[termWithIndex.index + 1]?.word in arrayOf("元", "块")) {      //单位q
-                            decorderResult = termWithIndex.value?.word + segment[termWithIndex.index + 1]?.word
+                            decorderResult = numberStr + "元"
                             break;
                         }
                     }
                 }
             }
 
-            it.decoderRes = decorderResult
+            it.decoderRes = (decorderResult ?: "- -")
         }
 
         rv_decode.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
